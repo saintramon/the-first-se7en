8897e2a05882ad00f2e3922b91abdd094b228216
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-03-12 19:25:20 +0800

    Fixed routes and import statements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import {useState, useEffect} from 'react';
-import axios from "axios";
-import './App.css'
+import React from 'react'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const apiCall = () => {
-  axios.get('http://localhost:8080').then((data) => {
-    //this console.log will be in our frontend console
-    console.log(data)
-  })
-}
+// import pages here
+import Home from './pages/home';
+import Login from './pages/login';
+import Signup from './pages/signup';
+import Instructions from './pages/instructions';
+import Quest from './pages/quest';
+import BonusQuest from './pages/bonus_quest';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <button onClick={apiCall}>Make API Call</button>
-
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route> path="/" element={<Home />} </Route>
+        <Route> path="/login" element-{<Login />}</Route>
+        <Route> path="/signup" element-{<Signup />}</Route>        
+        <Route> path="/instructions" element-{<Instructions />}</Route>        
+        <Route> path="/quest" element-{<Quest />}</Route>
+        <Route> path="/bonus_quest" element-{<BonusQuest />}</Route>               
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
