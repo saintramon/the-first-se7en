8faffa86e3ddf0fc3421b1b239a503b6e9799f2e
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-03-12 20:10:41 +0800

    Fixed server.js to reflect backend data to frontend

---
+++ client/src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import XPBar from '../components/xp_bar';
+
+function home() {
+  return (
+    <XPBar>
+        <p>Hello</p>
+    </XPBar>
+  )
+}
+
+export default home
