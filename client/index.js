e9628a3eca4e19a38ff3bd3dfac74cbbb2dbeae6
Rithik Tank <2233293@slu.edu.ph>
2025-03-12 22:22:49 +0800

    Changed client startup to use vite

--- client/index.js
+++ client/index.js
@@ -3,3 +3,12 @@ const app = express();
 const port = 3000;
 // Listen for requests on port
 app.listen(port);
+
+
+app.get("/HelloWorld", (req, res) => {
+    res.json({
+        groupName : "Client Side",
+        food: "food"
+
+    })
+});
--- server/server.js
+++ server/server.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const conn = require("./Connection");
-
+/*
 const corsOptions = {
     origin: ['https://localhost:5173']
 };
 app.use(cors(corsOptions));
+*/
+
+app.listen(8080, () => {
+    console.log("Server started on port 8080");
+});
 
 app.get("/HelloWorld", (req, res) => {
     res.json({
@@ -14,7 +19,4 @@ app.get("/HelloWorld", (req, res) => {
     })
 });
 
-app.listen(8080, () => {
-    console.log("Server started on port 8080");
-});
 
