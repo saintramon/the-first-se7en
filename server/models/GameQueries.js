c344db65ebf1a9eac87aef1e85daa905438788c7
Rithik Tank <2233293@slu.edu.ph>
2025-03-15 17:06:50 +0800

    added a missing query

--- server/models/GameQueries.js
+++ server/models/GameQueries.js
@@ -35,11 +35,6 @@ function validateUser(username, password, callback) {
     return performQuery(query, [username, password], callback);
 }
 
-function signUp(username, password, xp = 5, callback) {
-    var query = "INSERT INTO player (username, password, xp_level) VALUES(?,?,?)";
-    return performQuery(query, [username, password, xp], callback);
-}
-
 function getPlayerXP(playerID, callback) {
     var query = "SELECT xp_level FROM player WHERE player_id = ?";
     return performQuery(query, playerID, callback);
@@ -141,6 +136,5 @@ function getBonusQuestAns(bQuestID, callback) {
 
 module.exports =  {
     validateUser, getPlayerXP, xpUP, xpDown, getQuestImages, getQuestAnswer, 
-    getQuestDifficulty, getBonusQuestID, getBonusQuestInfo, getBonusQuestAns
-
+    getQuestDifficulty, getBonusQuestID, getBonusQuestInfo, getBonusQuestAns, getQuestInfo
 }
