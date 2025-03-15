33e0e8cca75b4e974b0d0cb657c82a00bb1c6549
saintramon <ramonjasmin8@gmail.com>
2025-03-16 07:34:32 +0800

    Added the get quest information in the model

--- server/models/QuestModel.js
+++ server/models/QuestModel.js
@@ -3,9 +3,32 @@ const {
     getQuestAnswer : getQuestAnswer,
 } = require('./GameQueries')
 
+function processQuestInfo(results) {
+    if (!Array.isArray(results)) {
+        console.error("Expected an array of results, but got:", results);
+        return []; // Return an empty array if the data isn't in the expected format
+    }
 
-function getQuestInformation() {
+    return results.map(quest => ({
+        questImages: [quest.img_1, quest.img_2, quest.img_3, quest.img_4], // Array of the images
+        questAnswer: quest.answer,
+        questDifficulty: quest.difficulty
+    }));
+}
 
+
+function getQuestInformation(questID) {
+    return new Promise((resolve, reject) => {
+        getQuestInfo(questID, (err, results) => {
+            if(err){
+                return reject(err);
+            }
+
+            console.log("Raw results: ", results);
+            const processedQuestInfo = processQuestInfo(results);
+            resolve(processedQuestInfo);
+        })
+    })
 }
 
 function submitAnswers() {
