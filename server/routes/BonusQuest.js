2fae42671e39fe9774889c7a2816e46a0422bbd4
saintramon <ramonjasmin8@gmail.com>
2025-03-18 21:22:09 +0800

    Fix: case sensitive issues in Routes

--- server/routes/BonusQuest.js
+++ server/routes/BonusQuest.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const bonusQuestModel = require('../models/bonusQuestModel')
+const bonusQuestModel = require('../models/BonusQuestModel')
 
 // When someone succesfully logs in get xp
 router.get('/', (req, res) => {
