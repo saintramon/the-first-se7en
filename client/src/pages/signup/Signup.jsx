3639fab7aa064e05f81fa6e8a4013e5259818098
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-03-18 11:15:50 +0800

    Finished signup page

--- client/src/pages/signup/Signup.jsx
+++ client/src/pages/signup/Signup.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import SignupForm from '../../components/form/SignupForm';
+import logo from '../../assets/icon/logo.png';
 
 function Signup() {
   return (
-    <div>Signup</div>
+    <div className='login-container'>
+      <img src={logo} alt='Litra2Code logo' className='logo'/>
+      <SignupForm />
+  </div>
   )
 }
 
-export default Signup
+export default Signup;
