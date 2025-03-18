339211623b54c75ceeb0e274f9195edcc24e1593
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-03-18 10:48:53 +0800

    Fixed axios configuration to have credentials set to true for cookies/session handling and to reference /api routes

--- client/axios.config.js
+++ client/axios.config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8080/';
-// axios.defaults.withCredentials = true;  <- unsure if needed
-
-export default axios;
+axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.withCredentials = true; 
+    
+export default axios;   
