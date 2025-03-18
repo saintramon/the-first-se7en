63c645d779668c6566a7adc732a556aca6d42049
MARVIN PATRICK LACANILAO <2230153@slu.edu.ph>
2025-03-18 09:06:27 +0800

    Changed naming conventions

---
+++ client/src/components/ui/XPBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./xp_bar.css";
+
+function XPBar({ xp }) {
+  return (
+    <div className="xp-bar">
+      {/* XP Star Icon */}
+      <div className="xp-star">
+        <img src="/xp-star.png" alt="XP Star" />
+      </div>
+
+      {/* XP Value */}
+      <span className="xp-value">{xp}</span>
+    </div>
+  );
+}
+
+export default XPBar;
