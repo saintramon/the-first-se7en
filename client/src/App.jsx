a6e723cd22c618e009768cdc8f10dea60847e782
saintramon <ramonjasmin8@gmail.com>
2025-03-18 10:38:58 +0800

    fix: added the missing catch clause after multiple merges

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,6 +22,7 @@ function App() {
           const response = await axios.get('/api/');
           console.log("API Response:", response.data); 
           setResponse(response.data);
+        }catch (error){
           console.error("Error fetching: ", error);
         }
     }
