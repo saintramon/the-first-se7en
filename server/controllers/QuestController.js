b787681cc0b361b47e5e5cf39fe63ca40525ec87
Rithik Tank <2233293@slu.edu.ph>
2025-03-19 09:07:58 +0800

    Wrote nitial quest button backend

--- server/controllers/QuestController.js
+++ server/controllers/QuestController.js
@@ -19,12 +19,36 @@ const generateLetterSet = (answer) => {
     return letterSet;
 };
 
+const removeLetter = (word, letters) => {
+    let letterSet = letters.split("");
+    let wordSplit = word.split("");
+
+    let randomIndex = Math.getRandomIndex(0, 20);
+    let letterToRemove = letterSet[randomIndex];
+
+    let currLength = wordSplit.length;
+    for (let x = 0; x < wordSplit.length; x++) {
+        if (wordSplit[y] == letterSet[x]) {
+            wordSplit.remove(wordSplit[y]); 
+            break;
+        }
+    }
+    if (wordSplit.length != currLength) return letterToRemove;
+    else return removeLetter(word, letters);
+}
+
+const revealLetter = (word) => {
+    index = Math.getRandomIndex(0, word.length);
+    return letterInfo[word.split("")[index], index];
+}
+
 const index = async (req, res) => {
     try {
         const questID = 1; // Hardcoded for testing
         const questInfoArray = await model.getQuestInformation(questID);
 
         if (!questInfoArray || questInfoArray.length === 0) {
+            return res.status(404).json( {error : "Message"});
             return res.status(404).json({ error: "Quest not found" });
         }
 
@@ -45,17 +69,43 @@ const index = async (req, res) => {
     }
 };
 
-// Placeholder functions to prevent "undefined" error
-const submitAnswers = (req, res) => {
+const verifyAnswer = (req, res) => {
+   // let correctAns = model.getQuestAnswer(req.questID);
+   // let userAnswer = req.answer;
+    if (model.getQuestAnswer(req.questID == req.answer)) {
+        res.status(200).json({
+            message: "Level Complete"
+        });
+    } else {
+        let attempts = req.attempts + 1;
+        res.status(200).json({
+            message: "Incorrect Answer",
+            attempts: attempts
+        })
+    }
     res.status(200).json({ message: "Submit Answers - Not Implemented Yet" });
 };
 
-const revealLetter = (req, res) => {
-    res.status(200).json({ message: "Reveal Letter - Not Implemented Yet" });
+const submitRevealLetter = (req, res) => {
+    try {
+        let revealedLetterIndex = revealLetter(req.word);
+        res.send( { reveleadLetter : revealedLetterIndex[0],
+            index : revealedLetterIndex[1]
+        }); 
+    } catch (Exception) {
+        res.status(500).json( { message: "Error revealing letter."})
+    }
+   // res.status(200).json({ message: "Reveal Letter - Not Implemented Yet" });
 };
 
-const removeLetter = (req, res) => {
+const submitRemoveLetter = (req, res) => {
+    removeLetter(req.word, req.letterSet);
     res.status(200).json({ message: "Remove Letter - Not Implemented Yet" });
 };
 
-module.exports = { index, submitAnswers, revealLetter, removeLetter };
+module.exports = { 
+    index, 
+    verifyAnswer, 
+    revealLetter : submitRemoveLetter, 
+    removeLetter : submitRevealLetter
+};
