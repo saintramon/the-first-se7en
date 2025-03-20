35a839a5530076a906ab3b4b860637efcdea1134
JOHN REY DE TORRES <118342555+JohnReyDT@users.noreply.github.com>
2025-03-20 23:10:28 +0800

    Created Home Page and its style (Beta Version). To be verified further by mr. Patrick Lacanilao

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import NavbarHome from '../../components/ui/NavbarHome';
+import PlayBtn from '../../components/ui/PlayBtn';
+import HowtoBtn from '../../components/ui/HowtoBtn';
+import "./home.css";
 
 function Home() {
   return (
-    <div>home</div>
+  <div className="home-container">
+
+    <div className="navbar-container">
+    <NavbarHome />
+    </div>
+
+    <div className="main-container">
+      <PlayBtn />
+      <HowtoBtn />
+    </div>
+
+  </div>
   )
 }
 
-export default Home
+export default Home;
