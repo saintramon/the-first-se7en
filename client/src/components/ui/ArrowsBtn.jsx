fd62bc44bfbfd445d05f42912a48f51fb92bfed9
JOHN REY DE TORRES <118342555+JohnReyDT@users.noreply.github.com>
2025-03-20 23:07:57 +0800

    Modified  components to remove conflicts

--- client/src/components/ui/ArrowsBtn.jsx
+++ client/src/components/ui/ArrowsBtn.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
-import prev from '../../assets/icon/prev.png'
-import next from '../../assets/icon/next.png'
-import './arrows.css'
+import React from 'react';
+import prev from '../../assets/icon/prev.png';
+import next from '../../assets/icon/next.png';
+import './Arrows.css';
 
 function ArrowsBtn() {
   return (
     <div>
-      <button class="arrow-btn">
+      <button class="btn">
         <img src={prev} alt="Previous"/>
       </button>
-      <button class="arrow-btn">
+      <button class="btn">
         <img src={next} alt="Next"/>
       </button>
     </div>
   );
 }
-
-export default ArrowsBtn
