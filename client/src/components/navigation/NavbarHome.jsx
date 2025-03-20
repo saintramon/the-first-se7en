fac8de840d052fceebfe2badc11e048e5987667b
MARVIN PATRICK LACANILAO <2230153@slu.edu.ph>
2025-03-21 02:13:29 +0800

    Fixed Navbar and home components

--- client/src/components/navigation/NavbarHome.jsx
+++ client/src/components/navigation/NavbarHome.jsx
@@ -1,22 +1,18 @@
 import React from "react";
-import GithubLogo from "../ui/GithubLogo";
-import Logo from "../ui/Logo";
+import GithubBtn from "../ui/GithubLogo";
 import LogOutBtn from "../navigation/LogOutBtn";
-import XPbar from "../navigation/XPBar";
+import XPbar from "./XPBar";
 import "../styles/navbar_home.css";
 
 const NavbarHome = () => {
   return (
-    <div className="navbar-home">
-      <div className="left-section">
-        <GithubLogo />
+    <div className="navbar">
+      <div className="left-icons">
+        <GithubBtn />
       </div>
-      <div className="logo-container">
-        <Logo />
-      </div>
-      <div className="right-section">
+      <div className="right-icons">
         <LogOutBtn />
-        <XPbar xp="100"/>
+        <XPbar xp="10"/>
       </div>
     </div>
   );
--- client/src/components/ui/GithubLogo.jsx
+++ client/src/components/ui/GithubLogo.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import githubLogo from '../../assets/icon/github-logo.png';
+import { Link } from "react-router-dom";
+import githubLogo from "../../assets/icon/github-logo.png";
 import "../styles/github_logo.css";
 
 function GithubLogo() {
   return (
-    <div>
-            <button class = "btn">
-             <img src={githubLogo} alt="GitHub"/> 
-            </button>
-    </div>
-  )
+    <Link to="https://github.com/saintramon/the-first-se7en" target="_blank" rel="noopener noreferrer">
+      <button className="github-btn">
+        <img src={githubLogo} alt="GitHub" />
+      </button>
+    </Link>
+  );
 }
 
 export default GithubLogo;
