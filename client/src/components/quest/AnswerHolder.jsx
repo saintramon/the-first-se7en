f1bde5c876326350cc80d03d5cdc0463d5c36eaa
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-03-22 01:41:24 +0800

    made answer holder takeback handler

--- client/src/components/quest/AnswerHolder.jsx
+++ client/src/components/quest/AnswerHolder.jsx
@@ -1,43 +1,56 @@
 import React from 'react';
 import "../styles/answer_holder.css";
 
-function AnswerHolder({ words }) {
-	const renderAnswerRows = () => {
-		if (words.length === 1) {
-			return (
-				<div id="answerHolder" className="answer-row" style={{ justifyContent: "center" }}>
-					{words[0].split("").map((word, index) => (
-						<div id={"div-" + index }key={index} className="answer-holder">
-							<h2 id={"h2-" + index}> 
-							</h2>
-						</div>
-					))}
-				</div>
-			);
-		} else if (words.length === 2) {
-			return (
-				<div id="answerHolder">
-					<div className="answer-row" style={{ justifyContent: "center" }}>
-						{words[0].split("").map((word, index) => (
-						<div id={"div-" + index }key={index} className="answer-holder">
-							<h2 id={"h2-" + index}> </h2>
-						</div>
-						))}
-					</div>
-					<div className="answer-row" style={{ justifyContent: "center" }}>
-						{words[1].split("").map((word, index) => (
-						<div id={"div-" + (words[0].length + index) }key={(words[0].length + index)} className="answer-holder">
-							<h2 id={"h2-" + (words[0].length + index)}> 
-							</h2>
-						</div>
-						))}
-					</div>
-				</div>
-			);
-		}
-	};
-
-	return <div className="answer-container">{renderAnswerRows()}</div>;
+function AnswerHolder({ words, onLetterReturn }) {
+  const renderAnswerRows = () => {
+    if (words.length === 1) {
+      return (
+        <div id="answerHolder" className="answer-row" style={{ justifyContent: "center" }}>
+          {words[0].split("").map((word, index) => (
+            <div 
+              id={"div-" + index} 
+              key={index} 
+              className="answer-holder"
+              onClick={() => onLetterReturn(index)}  // Add click handler for takeback
+            >
+              <h2 id={"h2-" + index}></h2>
+            </div>
+          ))}
+        </div>
+      );
+    } else if (words.length === 2) {
+      return (
+        <div id="answerHolder">
+          <div className="answer-row" style={{ justifyContent: "center" }}>
+            {words[0].split("").map((word, index) => (
+              <div 
+                id={"div-" + index} 
+                key={index} 
+                className="answer-holder"
+                onClick={() => onLetterReturn(index)}  // Add click handler for takeback
+              >
+                <h2 id={"h2-" + index}></h2>
+              </div>
+            ))}
+          </div>
+          <div className="answer-row" style={{ justifyContent: "center" }}>
+            {words[1].split("").map((word, index) => (
+              <div 
+                id={"div-" + (words[0].length + index)} 
+                key={(words[0].length + index)} 
+                className="answer-holder"
+                onClick={() => onLetterReturn(words[0].length + index)}  // Add click handler for takeback
+              >
+                <h2 id={"h2-" + (words[0].length + index)}></h2>
+              </div>
+            ))}
+          </div>
+        </div>
+      );
+    }
+  };
+  
+  return <div className="answer-container">{renderAnswerRows()}</div>;
 }
 
 export default AnswerHolder;
