24726b8bee1ba61abf1213192d3670d4d57ef7a4
MARVIN PATRICK LACANILAO <2230153@slu.edu.ph>
2025-03-21 15:14:49 +0800

    First page of instructions, prompt files

--- client/src/pages/instructions/Instructions.jsx
+++ client/src/pages/instructions/Instructions.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import NavbarHome from '../../components/navigation/NavbarHome'
+import Navbar from '../../components/navigation/Navbar'
 import ArrowsBtn from '../../components/buttons/ArrowsBtn'
 import "./instructions.css";
 
@@ -8,7 +8,7 @@ function Instructions() {
 
     <div className="instructions-container">
       <div className="navbar-container">
-        <NavbarHome />
+        <Navbar />
       </div>
 
     <div className="title-container">
@@ -16,19 +16,24 @@ function Instructions() {
         <h1>How to play Litra2Code?</h1>
       </div>
 
-      <div className="sub-title">
+      <div className="subtitle">
         <h2>Objective of the game</h2>
       </div>
     </div>
 
     <div className="content-container">
-      <p>LA quest displays four images linked to a certain personality, concept, or technology related to the 
-        history of programming languages. The player’s goal is to figure out the quest with a given set of letters beside the image. 
-        Answering a quest will give the player XP (5XP - Easy, 10 XP - Medium, 20 XP - Hard). </p>
+      <div className = "content">
+        <h4>A quest displays four images linked to a certain personality, concept, or technology related to the history of programming languages. 
+          The player’s goal is to figure out the quest with a given set of letters beside the image. Answering a quest will give the player XP 
+          (5XP - Easy, 10 XP - Medium, 20 XP - Hard).
+        </h4>
+          <hr />
+        <h4>A player is only given three chances to complete a quest. Failure to answer will result in a 5 XP deduction.
+        </h4>
         <hr />
-      <p>A player is only given three chances to complete a quest. Failure to answer will result in a 5 XP deduction.</p>
-        <hr />
-      <p>There are a total of 15 quests presented in a randomized order.</p>
+        <h4>There are a total of 20 quests presented in a randomized order.
+        </h4>
+      </div>
     </div>
 
     <div className="arrows-container">
