dc7437fa89ddcec56f127b4117748ac67aeadb99
JOHN REY DE TORRES <118342555+JohnReyDT@users.noreply.github.com>
2025-03-22 09:43:02 +0800

    Created Successprompt and edited the path of the css of Failprompt

---
+++ client/src/components/prompts/CorrectPrompt.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Next from '../../components/buttons/NextBtn';
+import XpIcon from '../../assets/icon/xp_bar.png';
+import SuccessBanner from '../../assets/icon/success.png';
+import '../styles/success.css';
+
+function CorrectPrompt({ xp }) {
+  return (
+    <div className="success-container">
+      <div className="xp-container">
+        <span>+{xp}</span>  
+        <img src={XpIcon} alt="XP Icon" />
+      </div> 
+
+      <div className="banner-container">
+        
+        <img src={SuccessBanner} alt="Success Banner" />
+      </div>
+
+      <div className="btn-container">
+        <Next />
+      </div>
+    </div>
+  );
+}
+
+export default CorrectPrompt;
