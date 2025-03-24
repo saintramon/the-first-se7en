71c0f44344b8a217d3c24f40027c3f70f310e26d
MARVIN PATRICK LACANILAO <2230153@slu.edu.ph>
2025-03-24 10:02:16 +0800

    Modified Submit button

--- client/src/components/buttons/SubmitBtn.jsx
+++ client/src/components/buttons/SubmitBtn.jsx
@@ -3,16 +3,12 @@ import '../styles/submit.css';
 
 function SubmitBtn({ onSubmitClick, disabled }) {
   return (
-    <div>
-      <button
-        id="btn-submit"
-        onClick={onSubmitClick}
-        disabled={disabled} 
-        className={disabled ? "disabled-btn" : ""} 
-      >
-        SUBMIT
-      </button>
-    </div>
+    <button
+      className="btn-submit"
+      onClick={onSubmitClick}
+      disabled={disabled}>
+      SUBMIT
+    </button>
   );
 }
 
